21eba1f4edc72f3f9b9238ee028a6c995e271706
tuunguyen99 <tuunguyen99@gmail.com>
2020-08-30 19:26:35 +0700

    Updates

---
+++ precache-manifest.e5f82c5acc3832932e4d5b0683e95b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807ecb9d500ab196f0b1c68ed33103dd",
+    "url": "/React_App/index.html"
+  },
+  {
+    "revision": "4c5bd0c6bb11177490fd",
+    "url": "/React_App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad29e83159a7c402038e",
+    "url": "/React_App/static/js/2.88265a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_App/static/js/2.88265a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5bd0c6bb11177490fd",
+    "url": "/React_App/static/js/main.fc7bd159.chunk.js"
+  },
+  {
+    "revision": "fa6f89dfab562f3d5c13",
+    "url": "/React_App/static/js/runtime-main.bd9bd8f0.js"
+  }
+]);
